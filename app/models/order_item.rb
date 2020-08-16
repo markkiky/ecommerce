5c9c0651736f93b98e7cd33249a9f171cb05f602
nyamburanjuguna <njugunajoan35@gmail.com>
2020-08-16 15:42:16 +0300

    add cart feature

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
     belongs_to :product
+
+    def order_total
+        price * quantity
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,8 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
 
   #cart routes 
-  get '/cart', to: 'order_items#index'
   resources :order_items, path: '/cart/items'
+  get '/cart', to: 'order_items#index'
   get '/cart/checkout', to: 'orders#new', as: :checkout
   patch '/cart/checkout', to: 'orders#create'
 
