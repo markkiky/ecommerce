73801e36ffb116c7ef9528751ead84e6c404f00e
nyamburanjuguna <njugunajoan35@gmail.com>
2020-09-18 12:09:09 +0300

    add order items table migrations

--- db/migrate/20200824124455_add_order_to_order_items.rb
+++ db/migrate/20200824124455_add_order_to_order_items.rb
@@ -1,5 +1,5 @@
 class AddOrderToOrderItems < ActiveRecord::Migration[6.0]
   def change
-    # add_reference :order_items, :order, null: false, foreign_key: true
+    add_reference :order_items, :order, null: false, foreign_key: true
   end
 end
