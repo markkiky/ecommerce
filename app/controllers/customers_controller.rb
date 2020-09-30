b1f9107902b9dd655e2c3affddba300d2274994b
nyamburanjuguna <njugunajoan35@gmail.com>
2020-09-30 18:28:16 +0300

    fix users page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,6 +5,7 @@ class CustomersController < ApplicationController
   # GET /customers.json
   def index
     @customers = Customer.all
+    @orders = Order.all
   end
 
   # GET /customers/1
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable
         #  :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :twitter]
          
